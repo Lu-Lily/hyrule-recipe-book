ee2b1c27dd8c7f8d5e77466405973ca0eedd9225
Lily Lu <lulily.contact@gmail.com>
2024-03-07 20:23:31 -0600

    get all food item data

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Hyrule Recipe Book
 
+This project makes use of the [Hyrule Compendium API](https://gadhagod.github.io/Hyrule-Compendium-API/#/)
+
 ### Technologies Used:
 * Python
 * Flask
--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+import requests
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    response = requests.get('https://botw-compendium.herokuapp.com/api/v3/compendium/category/creatures').json()
+    food_data = [food for food in response['data'] if food['edible'] is True]
+
+    return food_data
